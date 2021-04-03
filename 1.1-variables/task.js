093b559895f8b8d6c6326331c819f75979360912
Dmitry Lapshin <lapshin.dm56@gmail.com>
2021-04-03 13:01:25 +0300

    Task 1.1

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,26 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+	let sum = (algebra + geography + physics) / 3
+
+	return sum;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
+
+	return message;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let sum = (x * y + 5 * z + x - 1);
+
+    return sum;
 }
